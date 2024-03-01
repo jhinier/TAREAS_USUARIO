e0c4743006e56cf0e98aa7cb1ba827481070a690
jhinier <jhiniersanchez@gmail.com>
2024-03-01 16:34:32 -0500

    Cargando V1 'gestiontareas'

--- gestiontareas.sql
+++ gestiontareas.sql
@@ -1,3 +1,37 @@
 CREATE DATABASE gestionartareas;
 
 USE gestionartareas;
+
+-- Tabla de Usuarios
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_usuario VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    contraseña VARCHAR(50) NOT NULL,
+    correo_electronico VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Tabla de Categoría
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(50) NOT NULL
+);
+
+-- Tabla de Tareas
+CREATE TABLE tareas (
+    id_tarea INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_tarea VARCHAR(80) NOT NULL,
+    descripcion VARCHAR(600),
+    id_categoria INT NOT NULL,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabla de Tarea Usuario
+CREATE TABLE tareausuario (
+    completada BOOLEAN,
+    PRIMARY KEY(id_tarea, id_usuario),
+    id_tarea INT NOT NULL,
+    FOREIGN KEY (id_tarea) REFERENCES tareas(id_tarea) ON DELETE CASCADE ON UPDATE CASCADE,
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
